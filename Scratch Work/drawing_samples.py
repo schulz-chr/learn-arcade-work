cfc3e3a413dce98455e7af57df99e43824ad48ed
schulz-chr <schulz.chr@gmx.net>
2023-01-10 19:18:16 +0100

    adding the intro drawing samples

---
+++ Scratch Work/drawing_samples.py
@@ -0,0 +1,92 @@
+"""
+This is a sample program to show how to draw using the Python programming
+language and the Arcade library.
+
+Multi-line comments are surrounded by three double-quote marks.
+Single-line comments start with a hash/pound sign. #
+"""
+
+# Import the "arcade" library
+import arcade
+
+# Open up a window.
+# From the "arcade" library, use a function called "open_window"
+# Set the dimensions (width and height)
+# Set the window title to "Drawing Example"
+arcade.open_window(600, 600, "Drawing Example")
+
+# Set the background color with csscolor
+arcade.set_background_color(arcade.csscolor.LIGHT_CYAN)
+
+# Or set the background color with RGB values tuple
+arcade.set_background_color((224, 255, 255))
+
+# Get ready to draw
+arcade.start_render()
+
+# Draw a rectangle
+# Left of 0, right of 599
+# Top of 300, bottom of 0
+arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0,  arcade.csscolor.GREENYELLOW)
+
+# Tree trunk
+# Center of 100, 320
+# Width of 20
+# Height of 60
+arcade.draw_rectangle_filled(100, 320, 20, 60, (118, 65, 52))
+
+# Tree top
+arcade.draw_circle_filled(100, 350, 30, (250, 159, 66))
+
+# Another tree, with a trunk and ellipse for top
+arcade.draw_rectangle_filled(200, 320, 20, 60, (42, 26, 31))
+arcade.draw_ellipse_filled(200, 370, 60, 80, (59, 82, 73))
+
+# Another tree, with a trunk and arc for top
+# Arc is centered at (300, 340) with a width of 60 and height of 100.
+# The starting angle is 0, and ending angle is 180. >> creates half of an ellipse
+arcade.draw_rectangle_filled(300, 320, 20, 60, (118, 65, 52))
+arcade.draw_arc_filled(300, 340, 60, 100, (250, 159, 66), 0, 180)
+
+# Another tree, with a trunk and triangle for top
+# Triangle is made of these three points:
+# (400, 400), (370, 320), (430, 320)
+arcade.draw_rectangle_filled(400, 320, 20, 60, (37, 17, 1))
+arcade.draw_triangle_filled(400, 400, 370, 320, 430, 320, (93, 103, 91))
+
+# Draw a tree using a polygon with a list of points
+arcade.draw_rectangle_filled(500, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_polygon_filled(((500, 400),
+                            (480, 360),
+                            (470, 320),
+                            (530, 320),
+                            (520, 360)
+                            ),
+                           arcade.csscolor.DARK_GREEN)
+
+# Draw a sun
+arcade.draw_circle_filled(500, 550, 40, arcade.color.YELLOW)
+
+# Rays to the left, right, up, and down
+arcade.draw_line(500, 550, 400, 550, arcade.color.YELLOW, 3)    # <500, 550, 400, 550> : starting point (x=500, y=550), end point (x=400, y=550)
+arcade.draw_line(500, 550, 600, 550, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 500, 450, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 500, 650, arcade.color.YELLOW, 3)
+
+# Diagonal rays
+arcade.draw_line(500, 550, 550, 600, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 550, 500, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 450, 600, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 450, 500, arcade.color.YELLOW, 3)
+
+# Draw text at (150, 230) with a font size of 24 pts.
+arcade.draw_text("Arbor Day - Plant a Tree!",
+                 150, 230,
+                 arcade.color.BLACK, 24)
+
+
+# Finish drawing
+arcade.finish_render()
+
+# Keep the window up until someone closes it.
+arcade.run()
